734a15d2116fc62e1f748acfb280acfbdd7cf00e
thekim12 <thekim16@naver.com>
2023-11-10 22:21:24 +0900

    complete

--- RandomBingo/main.swift
+++ RandomBingo/main.swift
@@ -7,5 +7,34 @@
 
 import Foundation
 
-print("Hello, World!")
+var comChoice: Int = Int.random(in: 1...100)
+
+getClientNumber();
+
+
+func getClientNumber() -> Void {
+    var clientChoice: Int = 0
+    let clientInput = readLine()
+    if let clientNumber = clientInput {
+        if let number = Int(clientNumber){
+            clientChoice = number
+        }
+    }
+    judge(clientChoice: clientChoice)
+
+}
+
+
+func judge(clientChoice: Int ) -> Void {
+    if(clientChoice < comChoice){
+        print("UP")
+        getClientNumber()
+    } else if(clientChoice > comChoice){
+        print("DOWN")
+        getClientNumber()
+    }else{
+        print( "CORRECT")
+    }
+}
+
 
